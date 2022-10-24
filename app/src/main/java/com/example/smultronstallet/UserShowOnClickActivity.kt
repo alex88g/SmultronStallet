f641fca6f216990d9c62d0b25f5249d7e6eb6cb2
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2022-10-25 00:55:25 +0200

    jag har lagt en funtion i main som gör det möjligt att öppna ny aktivitet när man trycker på item

---
+++ app/src/main/java/com/example/smultronstallet/UserShowOnClickActivity.kt
@@ -0,0 +1,12 @@
+package com.example.smultronstallet
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UserShowOnClickActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_show_on_click)
+
+    }
+}
