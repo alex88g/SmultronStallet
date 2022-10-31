62927f8075f2115b4fde2963f0efe6cca5a80834
VladZGarcia <113976475+VladZGarcia@users.noreply.github.com>
2022-10-31 16:03:42 +0100

    Revert "login, recycleview"

---
+++ app/src/main/java/com/example/smultronstallet/Smultron_plats.kt
@@ -0,0 +1,5 @@
+package com.example.smultronstallet
+
+class SmultronPlats(var name:String, var review :String, var image:Int ) {
+
+}
---
+++ app/src/main/java/com/example/smultronstallet/User.kt
@@ -0,0 +1,4 @@
+package com.example.smultronstallet
+
+class User(var name: String?,var age: Int?=0) {
+}
