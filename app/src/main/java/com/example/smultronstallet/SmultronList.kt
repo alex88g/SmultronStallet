3df015bc7e5852605460234975e3dabcc8d74c54
Alexander Gallorini <alexandergallorini@MacBook.local>
2022-10-31 21:08:43 +0100

    login, recycle, fragment, map

--- app/src/main/java/com/example/smultronstallet/SmultronList.kt
+++ app/src/main/java/com/example/smultronstallet/SmultronList.kt
@@ -14,16 +14,16 @@ class SmultronList () {
     init {
 
 
-        listSmultronPerson.add(SmultronPlats("maria", "grym", R.drawable.a_chileansk))
-        listSmultronPerson.add(SmultronPlats("Görgen", "grym", R.drawable.b_indisk))
-        listSmultronPerson.add(SmultronPlats("Elias", "grym", R.drawable.c_vietnamesisk))
-        listSmultronPerson.add(SmultronPlats("cachito", "grym", R.drawable.d_chinesisk))
-        listSmultronPerson.add(SmultronPlats("Lea", "grym", R.drawable.e_japansk))
-        listSmultronPerson.add(SmultronPlats("Ernesto", "grym", R.drawable.f_balkan))
-        listSmultronPerson.add(SmultronPlats("Maria", "grym", R.drawable.g_libanesisk))
-        listSmultronPerson.add(SmultronPlats("Maria", "grym", R.drawable.h_rysk))
-        listSmultronPerson.add(SmultronPlats("Maria", "grym", R.drawable.i_italiensk))
-        listSmultronPerson.add(SmultronPlats("Maria", "grym", R.drawable.j_thai))
+//        listSmultronPerson.add(SmultronPlats("maria", "grym", R.drawable.a_chileansk))
+//        listSmultronPerson.add(SmultronPlats("Görgen", "grym", R.drawable.b_indisk))
+//        listSmultronPerson.add(SmultronPlats("Elias", "grym", R.drawable.c_vietnamesisk))
+//        listSmultronPerson.add(SmultronPlats("cachito", "grym", R.drawable.d_chinesisk))
+//        listSmultronPerson.add(SmultronPlats("Lea", "grym", R.drawable.e_japansk))
+//        listSmultronPerson.add(SmultronPlats("Ernesto", "grym", R.drawable.f_balkan))
+//        listSmultronPerson.add(SmultronPlats("Maria", "grym", R.drawable.g_libanesisk))
+//        listSmultronPerson.add(SmultronPlats("Maria", "grym", R.drawable.h_rysk))
+//        listSmultronPerson.add(SmultronPlats("Maria", "grym", R.drawable.i_italiensk))
+//        listSmultronPerson.add(SmultronPlats("Maria", "grym", R.drawable.j_thai))
 
 
         db.collection("users")
