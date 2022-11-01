27397de88f2ebf4f1255860eefb8606957877546
Alexander Gallorini <alexandergallorini@MacBook.local>
2022-11-01 08:15:29 +0100

    fix bug

--- app/src/main/java/com/example/smultronstallet/MainActivity.kt
+++ app/src/main/java/com/example/smultronstallet/MainActivity.kt
@@ -2,42 +2,28 @@ package com.example.smultronstallet
 
 
 
-
-import fragment.HomeFragment
-import fragment.MapsFragment
-import fragment.ProfileFragment
-import fragment.ReviewFragment
-import android.content.ContentValues
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import android.view.View
-import kotlinx.android.synthetic.main.activity_main.*
-import android.content.ContentValues.TAG
-import androidx.core.content.ContextCompat.startActivity
 import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
+import fragment.HomeFragment
+import fragment.MapsFragment
+import fragment.ReviewFragment
+import fragment.SearchFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
 
     private val homeFragment = HomeFragment()
-    private val profileFragment = ProfileFragment()
+    private val searchFragment = SearchFragment()
     private val mapsFragment = MapsFragment()
     private val reviewFragment = ReviewFragment()
-
     lateinit var button : Button
-
-    private var layoutManager: RecyclerView.LayoutManager?= null
-    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>?=null
 
     val db = Firebase.firestore
 
@@ -50,35 +36,23 @@ class MainActivity : AppCompatActivity() {
         replaceFragment(homeFragment)
 
 
-        
 
-//        getList {
-//
-//            var recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-//            recyclerView.layoutManager = LinearLayoutManager(this)
-//
-//            recyclerView.adapter = RecyclerAdapter(this,it)
-//        }
 
-   
         button = findViewById<Button>(R.id.buttonLog)
+
         button.setOnClickListener {
 
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
 
 
-            fun openNextActivity(view: View) {
 
-                val intent = Intent(this, SignInActivity::class.java)
-                startActivity(intent)
-            }
+
         }
-
         bottom_navigation.setOnNavigationItemSelectedListener {
             when(it.itemId){
                 R.id.ic_home -> replaceFragment(homeFragment)
-                R.id.ic_person_add -> replaceFragment(profileFragment)
+                R.id.ic_search -> replaceFragment(searchFragment)
                 R.id.ic_map -> replaceFragment(mapsFragment)
                 R.id.ic_rate_review -> replaceFragment(reviewFragment)
 
@@ -86,73 +60,15 @@ class MainActivity : AppCompatActivity() {
 
             true
         }
-
-
     }
 
 
-//    fun getList(myCallback :(MutableList<Place>) -> Unit) {
-//        db.collection("places")
-//            .get()
-//            .addOnCompleteListener {
-//                if(it.isSuccessful){
-//                    val list = mutableListOf<Place>()
-//                    for (document in it.result){
-//                        val name = document.data["name"].toString()
-//                        val review = document.data["review"].toString().toDouble()
-//                        val item = Place(name = name,review = review)
-//                        list.add(item)
-//                    }
-//                    myCallback(list)
-//                }
-//
-//            }
-//            .addOnFailureListener { exception ->
-//                Log.d(ContentValues.TAG, "error gettingdocuments: ", exception)
-//            }
-//    }
-
-
     fun replaceFragment(fragment: Fragment) {
-        if (fragment != null){
+        if (fragment != null) {
             val transaction = supportFragmentManager.beginTransaction()
             transaction.replace(R.id.fragment_container, fragment)
 
             transaction.commit()
         }
-
-
-        //var list = SmultronList().listSmultronPerson[5]
-
-
-        //var recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        //recyclerView.layoutManager = LinearLayoutManager(this)
-
-
-
-       //var adapter =RecyclerAdapter()
-       //recyclerView.adapter =adapter
-       //adapter.setOnItemClickListener(object: RecyclerAdapter.onItemClicklisterner {
-       //    override fun onItemClick(position: Int) {
-       //        //  Toast.makeText(this@MainActivity,"you clicked on item ${position}", Toast.LENGTH_LONG).show()
-
-       //        val intent= Intent(this@MainActivity,userNewActivity::class.java)
-
-       //        startActivity(intent)
-
-
-       //    }
-
-       //})
-
-
-
-  //  }
-
     }
-
 }
-
-
-
-
--- app/src/main/java/com/example/smultronstallet/SignUpActivity.kt
+++ app/src/main/java/com/example/smultronstallet/SignUpActivity.kt
@@ -3,7 +3,6 @@ package com.example.smultronstallet
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import com.example.smultronstallet.databinding.ActivitySignUpBinding
@@ -15,25 +14,24 @@ class SignUpActivity : AppCompatActivity() {
 
     lateinit var binding: ActivitySignUpBinding
     lateinit var auth: FirebaseAuth
-    lateinit var emailView : EditText
-    lateinit var passwordView : EditText
+    lateinit var emailView: EditText
+    lateinit var passwordView: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         emailView = findViewById(R.id.emailEt)
         passwordView = findViewById(R.id.passET)
 
 
         auth = Firebase.auth
-
-        binding = ActivitySignUpBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         auth = FirebaseAuth.getInstance()
 
+
         binding.textView2.setOnClickListener {
-            val intent = Intent(this, SignInActivity:: class.java)
+            val intent = Intent(this, SignInActivity::class.java)
 
             startActivity(intent)
         }
@@ -42,45 +40,25 @@ class SignUpActivity : AppCompatActivity() {
             val pass = binding.passET.text.toString()
             val confirmPass = binding.confirmPassEt.text.toString()
 
-            if (auth.currentUser == null) {
-                Log.d("!!!", "${auth.currentUser?.email}")
-
-            }
-
 
             if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
-
                 if (pass == confirmPass) {
-                    auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener (this) { task ->
-                        if (task.isSuccessful) {
 
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d("!!!", "createUserWithEmail:success")
-
+                    auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this) {
+                        if (it.isSuccessful) {
                             val intent = Intent(this, SignInActivity::class.java)
-
                             startActivity(intent)
-
-
                         } else {
 
-                            // If sign in fails, display a message to the user.
-                            Log.w("!!!", "createUserWithEmail:failure ${task.exception}")
-
-                            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT)
-                                .show()
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
                         }
                     }
-
-
                 } else {
-
-                    Toast.makeText(this, "Pasword does not mach", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Incorrect password", Toast.LENGTH_SHORT).show()
                 }
-
-
             } else {
-                Toast.makeText(this, "there are empty fields", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Empty fields are not allowed!", Toast.LENGTH_SHORT).show()
+
             }
         }
     }
