297c4ab7f5e14c0c1b0bad7e015803b01c27bf03
VladZGarcia <vladi.garcia@gmail.com>
2022-11-08 00:10:07 +0100

    ownerviewadd

--- app/src/main/java/Maps/Place.kt
+++ app/src/main/java/Maps/Place.kt
@@ -1,4 +1,6 @@
 package Maps
 
-class Place(var name : String ="",var latitude: Double?=0.0,var longitude :Double?=0.0,var review : Double?=0.0, var imageURL:String ="" ){
+import com.google.firebase.firestore.DocumentId
+
+class Place(@DocumentId var docId : String? = null, var name : String ="",var latitude: Double?=0.0,var longitude :Double?=0.0,var review : String? =null, var imageURL:String? =null ){
 }
