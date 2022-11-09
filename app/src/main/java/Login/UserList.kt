153cae16909ad55d6f21c5373d4540ee9b6a3eff
VladZGarcia <vladi.garcia@gmail.com>
2022-11-09 15:59:18 +0100

    db smultron add

--- app/src/main/java/Login/UserList.kt
+++ app/src/main/java/Login/UserList.kt
@@ -1,5 +1,6 @@
 package Login
 
+import Maps.PlaceList
 import android.content.ContentValues.TAG
 import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
@@ -8,18 +9,18 @@ import com.google.firebase.ktx.Firebase
 
 class UserList() {
     val db = Firebase.firestore
-
+    val placelist = PlaceList()
     var users = mutableListOf<User>()
 
 
     init {
-        createUsers()
+        //createUsers()
     }
 
     fun createUsers() {
-        for(i in 1..20){
+        for(i in 1..30){
             var randomNr = (1000000..9999999).random()
-            users.add(User("user$i","user${i}@user${i}mail.se","+4673$randomNr"))
+            users.add(User("","user$i","user${i}@user${i}mail.se","+4673$randomNr"))
 
 
         }
@@ -28,10 +29,16 @@ class UserList() {
                 .add(user)
                 .addOnSuccessListener { documentReference ->
                     Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                    for(i in 1..5) {
+                        var rndPlace = placelist.randomPlace()
+                        db.collection("users").document(documentReference.id)
+                            .collection("smultrons")
+                            .add(rndPlace)
+                        .addOnSuccessListener { documentReference ->
+                            Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
 
-
-
-
+                    }
+                    }
                 }
                 .addOnFailureListener { e ->
                     Log.w(TAG, "Error adding document", e)
