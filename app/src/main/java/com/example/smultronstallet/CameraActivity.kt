8cfed28718eee3cdd0672b9a21eed91aa0f21999
Alexander Gallorini <alexandergallorini@MacBook.local>
2022-11-17 09:59:16 +0100

    fix

--- app/src/main/java/com/example/smultronstallet/CameraActivity.kt
+++ app/src/main/java/com/example/smultronstallet/CameraActivity.kt
@@ -1,47 +1,66 @@
 package com.example.smultronstallet
-
 import Maps.MapsPlace
 import android.app.Activity
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_review.*
-
-
 private const val REQUEST_CODE = 42
 
 @Suppress("DEPRECATION")
-class CameraActivity : AppCompatActivity() {
+class CameraActivity : AppCompatActivity(){
     lateinit var smultronPlats: EditText
     lateinit var recenssion: EditText
-    lateinit var locationGps1: EditText
-    lateinit var bTagg: EditText
-    lateinit var buttonSave: Button
+    lateinit var ButtonSave: Button
+    lateinit var cameraButton1: ImageButton
+
+
+
     var newListOfSmultron = mutableListOf<MapsPlace>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_review)
-
-
-
-        smultronPlats = smultronPlace1
-        recenssion = ReviewS
-        locationGps1 = locationGps
-        bTagg = bussnestagg
-        buttonSave = buttonSmultronSave
         val db = Firebase.firestore
-        buttonSave.setOnClickListener {
+        smultronPlats = DittsmultronPlace1
+        recenssion = ReviewS
+        ButtonSave = buttonSmultronSave
+        cameraButton1 = findViewById(R.id.cameraButtonSmultron)
+
+
+        1
+        cameraButton1.setOnClickListener{
+            val intentCamerar = Intent(this, CameraActivity::class.java)
+            startActivity(intentCamerar)
+        }
+
+        ButtonSave.setOnClickListener {
+            var newSmultronPlace =
+                MapsPlace(name = smultronPlats.text.toString(), review = recenssion.text.toString())
+            db.collection("newSmultronPlace").add(newSmultronPlace)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                    finish()
+                }
 
 
         }
+
+        //  cameraButtonSmultron.setOnClickListener{
+        //val intentCamerar = Intent(this, CameraActivity::class.java)
+        //startActivity(intentCamerar)
+        //}
 
         val takePicturesIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         if (takePicturesIntent.resolveActivity(this.packageManager) != null) {
@@ -54,6 +73,23 @@ class CameraActivity : AppCompatActivity() {
 
     }
 
+    /*  private fun getLocation() {
+          locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+          if ((ContextCompat.checkSelfPermission(
+                  this,
+                  Manifest.permission.ACCESS_FINE_LOCATION
+              ) != PackageManager.PERMISSION_GRANTED)
+          ) {
+              ActivityCompat.requestPermissions(
+                  this,
+                  arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                  locationPermissionCode
+              )
+          }
+          locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5f, this)
+      }*/
+
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
             val takenImage = data?.extras?.get("data") as Bitmap
@@ -64,4 +100,7 @@ class CameraActivity : AppCompatActivity() {
         }
 
     }
+
+
+
 }
